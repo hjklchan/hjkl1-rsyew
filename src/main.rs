4eaf44e34536365b9f385e4435fb941ddefcb16d
Lucas chen <lucas.chen@gbo-asia.com>
2024-09-25 13:51:31 +0800

    :necktie: add global app context provider

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,29 @@ use hjkl1_yew::router::{switch, Router};
 use yew::prelude::*;
 use yew_router::{BrowserRouter, Switch};
 
+#[derive(Debug, Clone, PartialEq)]
+pub struct AppContext {
+    pub document_title: String,
+}
+
 #[function_component(App)]
 fn app() -> Html {
+    let app_ctx: UseStateHandle<AppContext> = use_state(|| AppContext {
+        document_title: "hjkl1 app".to_string()
+    });
+
     html! {
         <>
-            <BrowserRouter>
-                <Header />
-                <main class="overflow-y-auto p-6 font-serif">
-                    <Switch<Router> render={switch} />
-                </main>
-                <Footer />
-            </BrowserRouter>
+            // Global context data
+            <ContextProvider<AppContext> context={(*app_ctx).clone()}>
+                <BrowserRouter>
+                    <Header />
+                    <main class="overflow-y-auto p-6 font-serif">
+                        <Switch<Router> render={switch} />
+                    </main>
+                    <Footer />
+                </BrowserRouter>
+            </ContextProvider<AppContext>>
         </>
     }
 }
