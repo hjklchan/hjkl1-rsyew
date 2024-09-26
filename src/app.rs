c681faa93a5cb3e31676d5433a6de512d6d4650a
Lucas chen <lucas.chen@gbo-asia.com>
2024-09-26 15:44:16 +0800

    :sparkles: add server_status in app_context

--- src/app.rs
+++ src/app.rs
@@ -1,16 +1,54 @@
 use crate::app_ctx::AppContext;
+use crate::components::layouts::{Footer, Header};
 use crate::router::{switch, Router};
+use gloo::console::log;
+use gloo_net::http::Request;
 use yew::{
     function_component, html, use_effect_with, use_state, ContextProvider, Html, UseStateHandle,
 };
 use yew_router::{BrowserRouter, Switch};
-use crate::components::layouts::{Header, Footer};
 
 #[function_component(App)]
 pub fn app() -> Html {
     let app_ctx: UseStateHandle<AppContext> = use_state(|| AppContext {
         document_title: "Hjkl1 App".to_string(),
+        // None         =>  Checking
+        // Some(true)   =>  Available
+        // Some(false)  =>  Unavailable
+        server_status: None,
     });
+
+    // Check server status
+    //
+    // Listen api called `SERVER_ADDRESS/health`
+    {
+        let cloned_app_ctx = app_ctx.clone();
+
+        use_effect_with((), move |_| {
+            wasm_bindgen_futures::spawn_local(async move {
+                let response = Request::get("http://127.0.0.1:8000/health").send().await;
+                let mut tmp_app_ctx = (*cloned_app_ctx).clone();
+
+                match response {
+                    Ok(response) if response.status() == 200 => {
+                        tmp_app_ctx.server_status = Some(true);
+                        cloned_app_ctx.set(tmp_app_ctx);
+                        log!("server is ok");
+                    },
+                    Ok(_) => {
+                        tmp_app_ctx.server_status = Some(false);
+                        cloned_app_ctx.set(tmp_app_ctx);
+                        log!("server is unavailable1");
+                    },
+                    Err(_) => {
+                        tmp_app_ctx.server_status = Some(false);
+                        cloned_app_ctx.set(tmp_app_ctx);
+                        log!("server is unavailable2");
+                    },
+                }
+            });
+        })
+    }
 
     // Show app name in document title
     {
--- src/lib.rs
+++ src/lib.rs
@@ -7,5 +7,9 @@ pub mod app_ctx {
     #[derive(Debug, Clone, PartialEq)]
     pub struct AppContext {
         pub document_title: String,
+        // None         =>  Checking
+        // Some(true)   =>  Available
+        // Some(false)  =>  Unavailable
+        pub server_status: Option<bool>,
     }
 }
