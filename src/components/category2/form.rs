7bb8ea5617c5f566cbb07837ce3ed0ee24459d26
illusion <1161524405@qq.com>
2024-09-29 21:43:59 +0800

    :poop: improved category creation form

--- src/components/category2/form.rs
+++ src/components/category2/form.rs
@@ -12,31 +12,54 @@ pub struct FormProps {
 #[function_component(Form)]
 pub fn form(props: &FormProps) -> Html {
     let input_node_ref = use_node_ref();
-    
+
     let on_ok_click = {
-       let cloned_input_node_ref = input_node_ref.clone();
-       let clone_on_ok = props.on_ok.clone();
-        
+        let cloned_input_node_ref = input_node_ref.clone();
+        let clone_on_ok = props.on_ok.clone();
+
         Callback::from(move |_| {
             let input = cloned_input_node_ref.cast::<HtmlInputElement>();
-            let value = input.map(|html_input_element| html_input_element.value()).unwrap_or_default();
-            
+            let value = input
+                .map(|html_input_element| html_input_element.value())
+                .unwrap_or_default();
+
             clone_on_ok.emit(value);
+        })
+    };
+
+    let onblur = {
+        let cloned_on_close = props.on_close.clone();
+        
+        Callback::from(move |_| {
+            cloned_on_close.emit(());
         })
     };
 
     let on_close_click = {
         let cloned_on_close = props.on_close.clone();
-        
+
         Callback::from(move |_| cloned_on_close.emit(()))
     };
-    
+
+    {
+        let cloned_input_node_ref = input_node_ref.clone();
+
+        use_effect_with((), move |_| {
+            cloned_input_node_ref
+                .cast::<HtmlInputElement>()
+                .unwrap()
+                .focus()
+                .unwrap();
+        });
+    }
+
     html! {
         <div class="flex space-x-1">
             // <input class="p-1 border border-[#CDCDCD] focus:border-[#369]" />
             <div class="relative">
                 <input
                     ref={input_node_ref}
+                    {onblur}
                     type="text"
                     name="price"
                     id="price"
